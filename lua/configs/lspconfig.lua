5d8f61f180673e3166a7dc2ca3683eabe51f8563
BridgeDeck <littlefast2@gmail.com>
2026-05-26 16:00:54 +0800

    added gdscript

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "clangd", "pyright", "rust-analyzer", "luals" }
+local servers = { "html", "cssls", "clangd", "pyright", "rust-analyzer", "luals", "gdscript" }
 vim.lsp.enable(servers)
 
 
